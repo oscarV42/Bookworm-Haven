af3ed2ae6451de44c8651257df08e7b43f5c2fa9
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-28 14:34:00 -0400

    creating associations between Genre and Book model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,16 @@
 // import models
-const User = require('./tables/User')
-const Book = require('./tables/Book')
-const Post = require('./tables/Post')
-const Genre = require('./tables/Genre')
-const Comment = require('./tables/Comment')
-const Rate  = require('./tables/Rate')
+const User = require('./tables/User');
+const Book = require('./tables/Book');
+const Post = require('./tables/Post');
+const Genre = require('./tables/Genre');
+const Comment = require('./tables/Comment');
+const Rate  = require('./tables/Rate');
+
+Book.belongsTo(Genre, {
+    foreignKey: 'genre_id'
+})
+
+Genre.hasMany(Book, {
+    foreignKey: 'genre_id',
+    onDelete: 'CASCADE'
+})
