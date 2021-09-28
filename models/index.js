7e3569f1af21dcaf8b6b02aeb98096d1f5d0570f
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-28 14:46:13 -0400

    creating association between User and Comment model

--- models/index.js
+++ models/index.js
@@ -31,3 +31,12 @@ User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 })
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
