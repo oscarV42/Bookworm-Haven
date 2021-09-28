6060c6da367388b8d86735910117c482b000bf11
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-28 14:29:55 -0400

    importing models

--- models/index.js
+++ models/index.js
@@ -1,1 +1,7 @@
 // import models
+const User = require('./tables/User')
+const Book = require('./tables/Book')
+const Post = require('./tables/Post')
+const Genre = require('./tables/Genre')
+const Comment = require('./tables/Comment')
+const Rate  = require('./tables/Rate')
