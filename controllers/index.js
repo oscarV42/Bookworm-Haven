01deec36a6a96f2530e250c9e3620d54021d0cf4
unknown <nayefkanaan03@gmail.com>
2021-09-29 05:06:22 +0300

    Adding new routes to index

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,15 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dash-routes');
+const blogRoutes = require('./blog-routes');
+const searchRoutes = require('./search-routes');
+
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/blogs', blogRoutes); 
+router.use('/dashboard', dashRoutes)
+router.use('/search', searchRoutes);
 
 module.exports = router;
