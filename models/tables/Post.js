f51096b4a39caceebf0630d44457792f6db2e1e3
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-30 18:37:01 -0400

    implementing another foreign key in Post.js to reference a books id

--- models/tables/Post.js
+++ models/tables/Post.js
@@ -27,6 +27,14 @@ Post.init(
           unique: 'false'
         }
       },
+    book_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'book',
+        key: 'id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
