6eeabb2fcdf75cf973c624f538bcbe0030920d07
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-10-02 13:50:58 -0400

    importing seed data to be initialized when doing so in the terminal

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,25 @@
 const sequelize = require('../config/connection');
+const seedBook = require('./bookData');
+const seedComment = require('./commentData');
+const seedGenre = require('./genreData');
+const seedPost = require('./postData');
+const seedUser = require('./userData');
+const seedRate = require('./rateData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedGallery();
+  await seedGenre();
 
-  await seedPaintings();
+  await seedUser();
+
+  await seedBook();
+
+  await seedRate();
+
+  await seedPost();
+
+  await seedComment();
 
   process.exit(0);
 };
