1404c29d9b2a5bbd1f9567d91c318c54d1e54780
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-10-02 12:55:42 -0400

    creating Rate data to be seeded into the database

--- seeds/rateData.js
+++ seeds/rateData.js
@@ -3,28 +3,33 @@ const { Rate } = require('../models');
 const rateData = [
     {
         rate: 4,
-        reason: '',
+        reason: "Very much enjoyed this novel and it's message!",
         user_id: 1,
+        book_id: 1
     },
     {
         rate: 4,
-        reason: '',
-        user_id: 1,
+        reason: "I had to read this book for class, and I will say I actually enjoyed it a lot. ",
+        user_id: 2,
+        book_id: 2
+    },
+    {
+        rate: 5,
+        reason: 'This book has everything!',
+        user_id: 2,
+        book_id: 3
+    },
+    {
+        rate: 5,
+        reason: "I couldn't get enough of this book. King's writing is superb. It was simple, yet complex.",
+        user_id: 3,
+        book_id: 4
     },
     {
         rate: 4,
-        reason: '',
-        user_id: 1,
-    },
-    {
-        rate: 4,
-        reason: '',
-        user_id: 1,
-    },
-    {
-        rate: 4,
-        reason: '',
-        user_id: 1,
+        reason: 'A Tale of Two Cities is a definite favorite of mine. Despite being published in 1859, it still has the ability to draw the readier into the story and engage the mind.',
+        user_id: 5,
+        book_id: 6
     },
 ];
 
