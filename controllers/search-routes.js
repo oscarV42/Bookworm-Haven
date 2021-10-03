7b720a05eb2de277cfe9879450adc036f7aa3288
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-10-03 15:06:41 -0400

    Moved Api key to the .env file

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -18,8 +18,8 @@ router.get('/title/:title', async (request, response) => {
   try {
     const title = request.params.title;
     console.log("Book title", title)
-    //const api_key = process.env.API_KEY;
-    const api_url = `https://www.googleapis.com/books/v1/volumes?q=*+intitle:${title}&key=AIzaSyA7CGbTzigYmYn5KD6mT_WGZwuHFSHE2p4`;
+    const api_key = process.env.API_KEY;
+    const api_url = `https://www.googleapis.com/books/v1/volumes?q=*+intitle:${title}&key=${api_key}`;
     const fetch_response = await fetch(api_url);
     const json = await fetch_response.json();
     response.json(json);
@@ -34,8 +34,8 @@ router.get('/genre/:genre', async (request, response) => {
   try {
     const genre = request.params.genre;
     console.log("Book genre", genre)
-    //const api_key = process.env.API_KEY;
-    const api_url = `https://www.googleapis.com/books/v1/volumes?q=*+subject:${genre}&key=AIzaSyA7CGbTzigYmYn5KD6mT_WGZwuHFSHE2p4`;
+    const api_key = process.env.API_KEY;
+    const api_url = `https://www.googleapis.com/books/v1/volumes?q=*+subject:${genre}&key=${api_key}`;
     const fetch_response = await fetch(api_url);
     const json = await fetch_response.json();
     response.json(json);
