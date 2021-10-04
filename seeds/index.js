e52e9c7916c59f6da812ed91cebb22a424aa74c5
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-10-04 19:21:54 -0400

    updating index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,15 @@
 const sequelize = require('../config/connection');
 const seedBook = require('./bookData');
 const seedComment = require('./commentData');
-const seedGenre = require('./genreData');
 const seedPost = require('./postData');
 const seedUser = require('./userData');
-const seedRate = require('./rateData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedGenre();
-
   await seedUser();
 
   await seedBook();
-
-  await seedRate();
 
   await seedPost();
 
